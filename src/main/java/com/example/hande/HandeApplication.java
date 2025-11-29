f2a396681a103e757076b0b4ef1467b0cc1b7525
Francisco S. Lourenço <franciscolour2011@gmail.com>
2025-11-29 11:13:29 +0100

    Primeiras configurações

---
+++ src/main/java/com/example/hande/HandeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hande/HandeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
